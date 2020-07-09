a352a538a5f6f63a8b184b0d026441e486ab9de1
Ivan yu <gun123kk@gmail.com>
2020-07-09 23:55:59 +0800

    add test file for ranking and counting

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
 import rank_statistics_data as RSD
-file_path = './file/'
-
+import sys
+file_path = './rsfile/'
+file_selection = '0'
+if len(sys.argv) > 1 :
+    if sys.argv[1] == '0':
+        print("file selection: file with no comparison")
+    elif sys.argv[1] == '1':
+        file_path = './cfile/'
+        print("file selection: file with comparison")
 
 def run():
     rsd = RSD.rank_stcs_data(file_path)
